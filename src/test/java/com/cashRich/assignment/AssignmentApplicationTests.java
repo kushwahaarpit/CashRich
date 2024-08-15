e8a40ca823600028a986ae90a10d67f1cd024259
kushwahaArpit <arpit29062k@gmail.com>
2024-08-15 19:50:59 +0530

    user change

---
+++ src/test/java/com/cashRich/assignment/AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashRich.assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
