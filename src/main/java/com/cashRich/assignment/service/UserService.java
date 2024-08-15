8eb1279cdc58ec672c95b53c105523a2398642ad
kushwahaArpit <arpit29062k@gmail.com>
2024-08-15 22:13:30 +0530

    Password encoder injection updated

--- src/main/java/com/cashRich/assignment/service/UserService.java
+++ src/main/java/com/cashRich/assignment/service/UserService.java
@@ -5,6 +5,7 @@ import com.cashRich.assignment.entity.User;
 import com.cashRich.assignment.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,16 +17,22 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public User signup(User user) {
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
     public Optional<User> login(String username, String password) {
         Optional<User> user = userRepository.findByUsername(username);
-        if (user.isPresent() && passwordEncoder.matches(password, user.get().getPassword())) {
+        if (user.isPresent() && bCryptPasswordEncoder.matches(password, user.get().getPassword())) {
             return user;
         }
         return Optional.empty();
